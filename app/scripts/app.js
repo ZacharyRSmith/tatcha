2845529aaa376e2c109e4d2306e8654861ea4379
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-02-18 17:37:06 -0800

    (feat) Enable display of album thumbnails

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,18 +5,30 @@ $(function() {
     // albumsCache will have keys of userIds associated with values of
     // arrays of albums 
     albumsCache: { },
+    // photosCache will have keys of albumIds associated with values of
+    // arrays of photos
+    photosCache: { },
 
     init: function() {
       // Leave #stopSpinner in case of the removal of other methods called in
       // #init that invoke #stopSpinner
-      app.stopSpinner();
+      app.startSpinner();
       app.setAlbumsCache();
+      // FIXME #setAlbumsCache will stopSpinner() after
+      // #setPhotosCache has invoked #startSpinner, but before it has
+      // invoked #stopSpinner.  So, the spinner stops prematurely.
+      app.setPhotosCache();
 
       // Cache jQuery selectors
+      app.$currentAlbum = $('#currentAlbum');
       app.$currentAlbums = $('#currentAlbums');
       app.$getAlbumByUserId = $('#getAlbumByUserId');
 
       // Add listeners
+      app.$currentAlbums.on('click', 'li', function () {
+        var albumId = $(this).attr('id').match(/[0-9]+/);
+        app.setCurrentAlbum(albumId);
+      });
       app.$getAlbumByUserId.on('click', function () {
         app.getAlbumByUserId(1);
       });
@@ -24,7 +36,8 @@ $(function() {
     getAlbumByUserId: function (userId) {
       if (!app.albumsCache[userId]) return;
       var newCrntAlbums = app.albumsCache[userId].reduce(function (newCrntAlbums, album) {
-        return newCrntAlbums + '<li>' + album.title + '</li>';
+        var album = '<li id="album' + album.id + '">' + album.title + '</li>';
+        return newCrntAlbums + album;
       }, '');
 
       app.$currentAlbums.html(newCrntAlbums);
@@ -39,6 +52,39 @@ $(function() {
         albums.forEach(function (album) {
           app.albumsCache[album.userId] = app.albumsCache[album.userId] || [];
           app.albumsCache[album.userId].push(album);
+        });
+      }).fail(function (errMsg, errObj) {
+        errMsg = errMsg || "None given.";
+        errObj = errObj || "None given.";
+        console.error("Error message: ", errMsg);
+        console.error("Error object: ", errObj);
+      }).always(function () {
+        app.stopSpinner();
+      });
+    },
+    setCurrentAlbum: function (albumId) {
+      if (!app.photosCache[albumId]) return;
+      var newCrntAlbum = app.photosCache[albumId].reduce(function (newCrntAlbum, photo) {
+        var photo = '<li id="photo' + photo.id + '">' +
+                      '<h5>' + photo.title + '</h5>' +
+                      '<img alt="' + photo.title + '"' +
+                           'src="' + photo.thumbnailUrl + '" />' +
+                    '</li>';
+        return newCrntAlbum + photo;
+      }, '');
+
+      app.$currentAlbum.html(newCrntAlbum);
+    },
+    setPhotosCache: function () {
+      app.startSpinner();
+
+      $.ajax({
+        url: app.root + '/photos',
+        method: 'GET'
+      }).done(function (photos) {
+        photos.forEach(function (photo) {
+          app.photosCache[photo.albumId] = app.photosCache[photo.albumId] || [];
+          app.photosCache[photo.albumId].push(photo);
         });
       }).fail(function (errMsg, errObj) {
         errMsg = errMsg || "None given.";
