2280d7ed694269b29f4de2f290bb950018c84801
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-02-18 16:45:53 -0800

    (feat) app#init sets app.albumsCache

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,27 +2,58 @@ var app;
 $(function() {
   app = {
     root: 'http://jsonplaceholder.typicode.com',
+    // albumsCache will have keys of userIds associated with values of
+    // arrays of albums 
+    albumsCache: { },
 
     init: function() {
+      // Leave #stopSpinner in case of the removal of other methods called in
+      // #init that invoke #stopSpinner
       app.stopSpinner();
+      app.setAlbumsCache();
 
       // Cache jQuery selectors
-      app.$getPosts = $('#getPosts');
+      app.$getAlbumByUserId = $('#getAlbumByUserId');
 
       // Add listeners
-      app.$getPosts.on('click', app.getPosts);
+      app.$getAlbumByUserId.on('click', app.getAlbumByUserId(1));
     },
-    getPosts: function () {
+    getAlbumByUserId: function (userId) {
       app.startSpinner();
 
       $.ajax({
-        url: app.root + '/posts',
+        url: app.root + '/albums/' + userId,
         method: 'GET'
-      }).then(function(data) {
-        app.stopSpinner();
-
+      }).done(function (data) {
         console.log(data);
         return data;
+      }).fail(function (errMsg, errObj) {
+        errMsg = errMsg || "None given.";
+        errObj = errObj || "None given.";
+        console.error("Error message: ", errMsg);
+        console.error("Error object: ", errObj);
+      }).always(function () {
+        app.stopSpinner();
+      });
+    },
+    setAlbumsCache: function () {
+      app.startSpinner();
+
+      $.ajax({
+        url: app.root + '/albums',
+        method: 'GET'
+      }).done(function (albums) {
+        albums.forEach(function (album) {
+          app.albumsCache[album.userId] = app.albumsCache[album.userId] || [];
+          app.albumsCache[album.userId].push(album);
+        });
+      }).fail(function (errMsg, errObj) {
+        errMsg = errMsg || "None given.";
+        errObj = errObj || "None given.";
+        console.error("Error message: ", errMsg);
+        console.error("Error object: ", errObj);
+      }).always(function () {
+        app.stopSpinner();
       });
     },
     startSpinner: function(){
